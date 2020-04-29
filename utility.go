1dd3e5041a1c59e7d47857b6df1240498268a10e
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-04-29 19:29:09 +0530

    Listing Files using Utility Tool.

---
+++ utility.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+	"os/exec"
+)
+
+var (
+	app     = cli.NewApp()
+	myFlags = []cli.Flag{
+		&cli.StringFlag{
+			Name:    "argument",
+			Value:   "Enter the Argument",
+			Aliases: []string{"arg"},
+		},
+	}
+)
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatalln("Error::", err)
+	}
+}
+func info() {
+	app.Name = "Utility Cli"
+	app.Usage = "A tool for querying Linux Commands"
+	app.Version = "0.0.1"
+}
+func command() {
+	app.Commands = []*cli.Command{
+		{
+			Name:    "list",
+			Usage:   "Use to List Values",
+			Flags:   myFlags,
+			Aliases: []string{"l"},
+			Action: func(context *cli.Context) error {
+				cmd, err := exec.Command("ls", "-lrth").CombinedOutput()
+				handleError(err)
+				fmt.Println(string(cmd))
+				return nil
+			},
+		},
+	}
+}
+func main() {
+	info()
+	command()
+	err := app.Run(os.Args)
+	handleError(err)
+}
